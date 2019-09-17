8ada57d3533cab89d96b68fba3e28e62ebb81c37
Cornel <44235575+cornel001@users.noreply.github.com>
2019-09-17 10:54:40 +0300

    fetch for array of logins and promise all

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const mainUrl = `https://api.github.com/users`;
 fetch(`${mainUrl}?per_page=50`).then(
   response=>response.json()
 ).then(
-  users=>users.map(user=>`${mainUrl}/${user.login}`)
+  users=>users.map(user=>fetch(`${mainUrl}/${user.login}`))
+).then(
+  Promise.all
 ).then(
   console.log
 ).catch(
