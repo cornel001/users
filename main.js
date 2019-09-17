f3f6fa829e82a6b5dae4dab7a4710571f3f19c49
Cornel <44235575+cornel001@users.noreply.github.com>
2019-09-17 09:55:12 +0300

    object name correction

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ fetch('https://api.github.com/users?per_page=50').then(
   response=>response.json()
 ).then(
   users=>{
-    let logins = users.map(user=>users.login);
+    let logins = users.map(user=>user.login);
     console.log(logins);
   }
 ).catch(
