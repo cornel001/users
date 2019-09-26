6da758ba0a43984773c3b71528216130decafdde
Cornel <44235575+cornel001@users.noreply.github.com>
2019-09-26 09:07:20 +0300

    Special static list for basic display development

--- main.js
+++ main.js
@@ -1,26 +1,63 @@
 'use strict';
-const mainUrl = `https://api.github.com/users`;
 
-fetch(`${mainUrl}?per_page=50`).then(
-  response=>response.json()
-).then(
-  users=>users.map(
-    user=>fetch(`${mainUrl}/${user.login}`).then(response=>response.json()).then(user=>{
-      displayUserName(user.name); 
-      // console.log(user);
-    })
-  )
-).then(
-  promises=>Promise.all(promises)
-).catch(
-  displayUserName //for error, use same display
-)
-
-let container = document.getElementById('container');
 function displayUserName(userName) {
-  if (!userName) userName = '-'
-  let nameBox = document.createElement('div');
-  nameBox.innerHTML = userName;
-  container.appendChild(nameBox);
+    let container = document.getElementById('container');
+    let nameBox = document.createElement('div');
+    nameBox.innerHTML = userName;
+    container.appendChild(nameBox);
+}
+
+let names = [
+'PJ Hyett',
+'Chris Wanstrath',
+'Yehuda Katz',
+'Ezra Zygmuntowicz',
+'Michael D. Ivey',
+'Tom Preston-Werner',
+'Evan Phoenix',
+'Chris Van Pelt',
+'Bryan Helmkamp',
+'Wayne E. Seguin',
+'Kevin Clark',
+'risk danger olson',
+'Marc-André Cournoyer',
+'Toby Sterrett',
+'Geoffrey Grosenbach',
+'Justin Palmer',
+'Jesse Andrews',
+'Roland',
+'Tom ten Thij',
+'Kevin Williams',
+'Nitay Joffe',
+'Lukas Biewald',
+'Dave Fayram',
+'James Golick',
+'Corey Donohoe',
+'Jesse Newland',
+'Josh Knowles',
+'John Hornbeck',
+'Seth Fitzsimmons',
+'Jeff Whitmire',
+'Blake Mizerany',
+'Michael Buffington',
+'Rein Henrichs',
+'Kenzi Connor',
+'Britt Selvitelle',
+'Rob Sanheim',
+'Eivind Uggedal',
+'Bruce Williams',
+'Matt Mower',
+'Scott Chacon',
+'Abhay Kumar',
+'rabble',
+'Ben Burkert',
+'André Arko',
+'Sam Smoot',
+'Jamie van Dyke',
+'Ryan Dahl',
+];
+
+for (let name of names) {
+    displayUserName(name);
 }
 
